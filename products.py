1b957e559f0d0995a085ca9232bd48a2f1be82bd
sabuchen <t108ac8514@ntut.org.tw>
2021-05-01 22:11:04 +0800

    add encoding

--- products.py
+++ products.py
@@ -27,11 +27,12 @@ for p in products:
 	#再進階一點應用
 	print(p[0], '的價格是', p[1])
 
-with open('products.txt', 'w') as f:
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+# with open('products.txt', 'w') as f:
+# 	for p in products:
+# 		f.write(p[0] + ',' + p[1] + '\n')
 
-with open('products.csv', 'w') as f: 
+with open('products.csv', 'w', encoding='utf-8') as f: 
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
 
