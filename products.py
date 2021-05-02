3cefe0e570701812f5a275f9ae3cde8df14c0db0
sabuchen <t108ac8514@ntut.org.tw>
2021-05-02 11:07:06 +0800

    add os

--- products.py
+++ products.py
@@ -1,52 +1,32 @@
-#讀取檔案
-products = []
+#找檔案在不在
+import os
+products = [] 
 
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		#土方法把s取到的東西丟到name,price
-		# s = line.strip().split(',')
-		# name = s[0]
-		# price = s[1]
+if os.path.isfile('products.csv'):
+    print('找到檔案了')
+    with open('products.csv', 'r', encoding='utf-8') as f: #讀取檔案
+        for line in f:
+            if '商品,價格' in line: #更簡潔的寫
+               continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+	print('沒有找到檔案')
 
-		#更簡潔的寫
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
-
-
+#輸入購買的商品
 
 while True:
 	name = input('請輸入購買過的商品：')
 	if name == 'q':
 		break
 	price = input('請輸入價格：')
-	#苦幹型寫法
-	# p = []
-	# p.append(name)
-	# p.append(price)
-	# products.append(p)
-
-	#進階型寫法
-	# p = [name, price]
-	# products.append(p)
-
-	#超級強寫法
 	products.append([name, price])
 
 print(products)
 
 for p in products:
-	#印出小清單內的內容
-	#print(p)
-
-	#再進階一點應用
-	print(p[0], '的價格是', p[1])
-
-# with open('products.txt', 'w') as f:
-# 	for p in products:
-# 		f.write(p[0] + ',' + p[1] + '\n')
+	print(p[0], '的價格是', p[1]) #印出小清單內的內容
 
 with open('products.csv', 'w', encoding='utf-8') as f: 
 	f.write('商品,價格\n')
